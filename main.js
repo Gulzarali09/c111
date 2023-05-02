84947c4afcacefb3d1d769a3a264d382162f2536
Gulzarali09 <121237439+Gulzarali09@users.noreply.github.com>
2023-05-02 11:04:34 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="'+data_uri+'"/>';
+        });
+
+
+}
+
+
+
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/F4xhK0-du/model.json',modelLoaded);
+
+function modelLoaded(){
+    console.log('ml5 version:',ml5.version);
+}
